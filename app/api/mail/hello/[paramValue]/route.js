3e6de71a343181542833fe9f018caecad5c85c45
AdamCanard <azcunard@gmail.com>
2024-03-13 15:35:05 -0600

    temp code for qr

--- app/api/mail/hello/[paramValue]/route.js
+++ app/api/mail/hello/[paramValue]/route.js
@@ -1,13 +1,30 @@
 import { isRedirectError } from "next/dist/client/components/redirect";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
+import Test from "@/models/Test";
 
 export async function GET(request, { params }) {
   try {
     const cookie = cookies().get("role");
     const value = params.paramValue;
+    //if user doesnt have cookie, redirect to tickets
     if (!cookie) {
       redirect("/tickets", "push");
+    }
+    console.log(request);
+    //test cookie for bartender
+    //check if user exists
+    const test = await Test.exists({ ticketId: params.paramValue });
+    if (test === null) {
+      //if user doesnt exist (shouldnt be the case ever)
+      //create the user
+      console.log(params.paramValue + " is not a user");
+      //if the user does exist, increment raffle
+    } else {
+      console.log(params.paramValue + " is a user.");
+      const test2 = await Test.findById(test._id);
+      test2.raffle = test2.raffle + 1;
+      await test2.save();
     }
 
     return Response.json(
