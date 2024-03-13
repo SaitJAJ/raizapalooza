20c737c6ab654687306fdf0f81a89669748ac29c
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-03-12 22:00:11 -0600

    Dev Cookie & Endpoint

---
+++ app/api/mail/hello/[paramValue]/route.js
@@ -0,0 +1,22 @@
+import {isRedirectError} from "next/dist/client/components/redirect";
+import {redirect} from 'next/navigation'
+import {cookies} from "next/headers";
+export async function GET(request,{params}){
+    try{
+        const cookie = cookies().get('role')
+        const value= params.paramValue;
+        if(!cookie) {
+            redirect('/tickets','push')
+        }
+        return Response.json({value:value,cookie:cookie.value},{status:200})
+    }catch(error){
+        if(isRedirectError(error)){
+            throw error
+        }
+        console.log(error)
+
+        return Response.json("Something went wrong",{status:200})
+    }
+
+
+}
