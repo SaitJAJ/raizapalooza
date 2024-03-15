776a3bb745cd08d065ac27130f1c3ae22393993f
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-03-15 09:08:10 -0600

    Parallax Background WIP

--- app/layout.js
+++ app/layout.js
@@ -4,20 +4,30 @@ import CookieConsent from "@/components/CookieConsent";
 import {Suspense} from "react";
 import Loading from "@/components/Loading";
 import {getCookiePolicy} from "@/library/cookieServices";
+import ParallaxBackground from "@/components/layout/ParallaxBackground";
 
 export default async function RootLayout({children}){
     let policy = await getCookiePolicy()
+
     return(
         <html>
             <body>
-                <div className={'min-h-[100vh] pb-2.5 overflow-x-hidden'}>
-                    {children}
-                </div>
-                    <Suspense fallback={<Loading loading={true}/>}>
-                        <CookieConsent cookies={policy}/>
-                    </Suspense>
+
+
+            <div className={'min-h-[100vh] pb-2.5 overflow-x-hidden w-full absolute'} >
+                <ParallaxBackground>
+                    <div className={'fixed w-full'}>
+                        {children}
+                    </div>
+                </ParallaxBackground>
                 <Footer/>
-            </body>
+            </div>
+            <Suspense fallback={<Loading loading={true}/>}>
+                <CookieConsent cookies={policy}/>
+            </Suspense>
+
+                {/*</ParallaxBackground>*/}
+                </body>
         </html>
     )
 }
