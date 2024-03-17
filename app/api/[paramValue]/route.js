9d45b0dca0d3a858fd459d598154fcf8b605e411
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-03-16 18:44:34 -0600

    updates raffles when bartender scans

--- app/api/[paramValue]/route.js
+++ app/api/[paramValue]/route.js
@@ -1,10 +1,12 @@
 import { isRedirectError } from "next/dist/client/components/redirect";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
+import mongoose from "mongoose";
 import Ticket from "@/models/Ticket";
 
 export async function GET(request, { params }) {
   try {
+    mongoose.connect(process.env.MONGODB_URI);
     const cookie = cookies().get("role");
     const value = params.paramValue;
     //if user doesnt have cookie, redirect to tickets
@@ -25,6 +27,10 @@ export async function GET(request, { params }) {
       //let doc = await Ticket.findOneAndUpdate(filter, modify, { new: true });
       //doc will hold the updated row
       //console.log(doc);
+      const foundTicket = await Ticket.findOne({ticketId: request.headers.get("ticketId")});
+      foundTicket.raffle = foundTicket.raffle += 1;
+      await foundTicket.save();
+      console.log(foundTicket);
 
       //test cookie for bouncer
     } else if (cookie.value === "bouncer") {
