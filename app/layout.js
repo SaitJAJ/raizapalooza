3b08a1db2a3d02bb88ad436e9b01d59a2f036cac
AdamCanard <azcunard@gmail.com>
2024-03-16 20:58:55 -0600

    missing } from merge

--- app/layout.js
+++ app/layout.js
@@ -3,33 +3,33 @@ import Footer from "@/components/Footer";
 import CookieConsent from "@/components/CookieConsent";
 import { Suspense } from "react";
 import Loading from "@/components/Loading";
-import {getCookiePolicy} from "@/library/cookieServices";
+import { getCookiePolicy } from "@/library/cookieServices";
 import ParallaxBackground from "@/components/layout/ParallaxBackground";
 
-export default async function RootLayout({children}){
-    let policy = await getCookiePolicy()
+export default async function RootLayout({ children }) {
+  let policy = await getCookiePolicy();
 
-    return(
-        <html>
-            <body>
-            {/*<div className={'pb-2.5 overflow-x-hidden w-full'} >*/}
-                <div className={'w-full min-h-[100vh]'}>
-                    {children}
-                </div>
-                <Footer/>
-            {/*</div>*/}
-            <Suspense fallback={<Loading loading={true}/>}>
-                <CookieConsent cookies={policy}/>
-            </Suspense>
+  return (
+    <html>
+      <body>
+        {/*<div className={'pb-2.5 overflow-x-hidden w-full'} >*/}
+        <div className={"w-full min-h-[100vh]"}>{children}</div>
+        <Footer />
+        {/*</div>*/}
+        <Suspense fallback={<Loading loading={true} />}>
+          <CookieConsent cookies={policy} />
+        </Suspense>
 
-                {/*</ParallaxBackground>*/}
-                </body>
-        </html>
-    )
+        {/*</ParallaxBackground>*/}
+      </body>
+    </html>
+  );
 }
-export const metadata={
-    title: {
-        template:"%s | Raizapalooza  ",
-        default:'Raizapalooza'
-    },
-    description:"Get your tickets now for the next Raizapalooza, relive the memories and celebrate Raiza! "
+export const metadata = {
+  title: {
+    template: "%s | Raizapalooza  ",
+    default: "Raizapalooza",
+  },
+  description:
+    "Get your tickets now for the next Raizapalooza, relive the memories and celebrate Raiza! ",
+};
