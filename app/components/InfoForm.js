da757f74337aba992d906e77f77f675851b2f2e3
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-03-16 23:29:44 -0600

    Birthday Input, Sections all min-height 100% and style for Select

--- app/components/InfoForm.js
+++ app/components/InfoForm.js
@@ -6,15 +6,17 @@ import {addTicket} from "@/app/lib/ticketServices";
 import TextInput from "@/components/TextInput";
 import HiddenInput from "@/components/HiddenInput";
 import {useRouter} from "next/navigation";
+import BirthdayPicker from "@/components/BirthdayPicker";
 
 const InfoForm = forwardRef(function InfoForm({loading},formRef){
 
     return(
-        <form className={'w-full'} ref={formRef} name={"infoForm"} id={"infoForm"}>
+        <form className={'w-full min-h-[100vh] py-8'} ref={formRef} name={"infoForm"} id={"infoForm"}>
             <HiddenInput type={'text'} id={'tier'} value={'earlyBird'} hidden={true} className={'hidden'}/>
             <TextInput label={'Name'} type={"text"} id={'name'} placeholder={'Name'}/>
             <TextInput label={'Email'} type={"text"} id={"email"} placeholder={'Email'}/>
             <TextInput label={'Phone Number'} type={"text"} id={'phoneNumber'}  placeholder={'Phone Number'}/>
+            <BirthdayPicker label={'Birthday'} id={'birthday'} minDate={new Date('April 13, 2006')} />
             <Loading loading={loading}>
                 <Button type={'submit'} value={"Submit"}/>
             </Loading>
