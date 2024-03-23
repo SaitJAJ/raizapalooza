7554f031250b4f68c997f17df5263ee76968335b
AdamCanard <azcunard@gmail.com>
2024-03-22 18:16:45 -0600

    need to fix positioning and add title

--- app/whozapalooza/page.js
+++ app/whozapalooza/page.js
@@ -15,40 +15,51 @@ const josefin_sans = Josefin_Sans({
 
 export default function Page() {
   return (
-    <div className="flex w-full h-screen">
-      <WhoBox
-        name="VENDER"
-        src={whitePlaceholder}
-        alt="placeholder photo"
-        text="Lorem ipsum dolor sit sed do asdasd ascojn temporse sadasd deiusmod temporsed do eiusmod temporsed do eiusmod tempor "
-        website="https://hexdle-nine.vercel.app/"
-        instagram="instagram"
-      />
-      <WhoBox
-        className="flex items-end"
-        name="VENDER"
-        src={whitePlaceholder}
-        alt="placeholder photo"
-        text="Lorem ipsum dolor sit sed do asdasd ascojn temporse sadasd deiusmod temporsed do eiusmod temporsed do eiusmod tempor "
-        website="https://hexdle-nine.vercel.app/"
-        instagram="instagram"
-      />
-      <WhoBox
-        name="VENDER"
-        src={whitePlaceholder}
-        alt="placeholder photo"
-        text="Lorem ipsum dolor sit sed do asdasd ascojn temporse sadasd deiusmod temporsed do eiusmod temporsed do eiusmod tempor "
-        website="https://hexdle-nine.vercel.app/"
-        instagram="instagram"
-      />
-      <WhoBox
-        name="VENDER"
-        src={whitePlaceholder}
-        alt="placeholder photo"
-        text="Lorem ipsum dolor sit sed do asdasd ascojn temporse sadasd deiusmod temporsed do eiusmod temporsed do eiusmod tempor "
-        website="https://hexdle-nine.vercel.app/"
-        instagram="instagram"
-      />
+    <div className="flex flex-col items-center md:flex-row h-screen justify-around mt-16">
+      <div className="flex flex-col justify-center items-center md:grid md:grid-cols-2 md:grid-rows-2 mb-16 border-2 border-white">
+        <div className="md:col-start-1 md:row-start-1">
+          <WhoBox
+            name="VENDER"
+            src={whitePlaceholder}
+            alt="placeholder photo"
+            text="Lorem ipsum dolor sit sed do asdasd ascojn temporse sadasd deiusmod temporsed do eiusmod temporsed do eiusmod tempor "
+            website="https://hexdle-nine.vercel.app/"
+            instagram="instagram"
+          />
+        </div>
+        <div className="md:col-start-2 md:row-start-2">
+          <WhoBox
+            name="VENDER"
+            src={whitePlaceholder}
+            alt="placeholder photo"
+            text="Lorem ipsum dolor sit sed do asdasd ascojn temporse sadasd deiusmod temporsed do eiusmod temporsed do eiusmod tempor "
+            website="https://hexdle-nine.vercel.app/"
+            instagram="instagram"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center md:grid md:grid-cols-2 md:grid-rows-2 mt-16 border-2 border-white">
+        <div className="md:col-start-1 md:row-start-1">
+          <WhoBox
+            name="VENDER"
+            src={whitePlaceholder}
+            alt="placeholder photo"
+            text="Lorem ipsum dolor sit sed do asdasd ascojn temporse sadasd deiusmod temporsed do eiusmod temporsed do eiusmod tempor "
+            website="https://hexdle-nine.vercel.app/"
+            instagram="instagram"
+          />
+        </div>
+        <div className="md:col-start-2 md:row-start-2">
+          <WhoBox
+            name="VENDER"
+            src={whitePlaceholder}
+            alt="placeholder photo"
+            text="Lorem ipsum dolor sit sed do asdasd ascojn temporse sadasd deiusmod temporsed do eiusmod temporsed do eiusmod tempor "
+            website="https://hexdle-nine.vercel.app/"
+            instagram="instagram"
+          />
+        </div>
+      </div>
     </div>
   );
 }
@@ -56,7 +67,7 @@ export default function Page() {
 function WhoBox({ src, name, alt, text, website, instagram }) {
   return (
     <>
-      <div className="flex flex-row w-80 h-56 overflow-hidden">
+      <div className="flex flex-row w-80 h-56">
         <div className="flex flex-col justify-start items-start grow basis-1/2 overflow-hidden ">
           <Image width={150} height={150} src={src} alt={alt} />
           <h2 className={`flex ${antonio.className}`}>{name}</h2>
