28442ee619686fd18b45ad9742e4343061d23d67
AdamCanard <azcunard@gmail.com>
2024-03-22 22:20:46 -0600

    removed borders lol

--- app/whozapalooza/page.js
+++ app/whozapalooza/page.js
@@ -26,7 +26,7 @@ export default function Page() {
         id="outer"
         className="flex flex-col items-center md:pt-48 md:flex-row h-screen justify-around mt-16"
       >
-        <div className="absolute border-2 border-white text-center top-6 md:top-[20%]">
+        <div className="absolute text-center top-6 md:top-[20%]">
           <h1 className="font-tan-headline text-4xl md:text-6xl ">
             Whozapalooza
           </h1>
@@ -34,8 +34,8 @@ export default function Page() {
             This page gives a run down of every one there. Filler Text
           </p>
         </div>
-        <div className="border-4 border-element-1 flex flex-col gap-10 w-[90%] md:grid md:grid-cols-6 md:grid-rows-2 md:w-[80%] md:h-[35rem]">
-          <div className="border-2 order-white md:col-start-1 md:col-span-2 md:row-start-1 md:self-start">
+        <div className="flex flex-col gap-10 w-[90%] md:grid md:grid-cols-6 md:grid-rows-2 md:w-[80%] md:h-[35rem]">
+          <div className="md:col-start-1 md:col-span-2 md:row-start-1 md:self-start">
             <WhoBox
               name="VENDOR"
               src={whitePlaceholder}
@@ -45,7 +45,7 @@ export default function Page() {
               instagram="instagram"
             />
           </div>
-          <div className="border-2 border-white md:col-start-2 md:col-span-2 md:row-start-2 md:self-end">
+          <div className="md:col-start-2 md:col-span-2 md:row-start-2 md:self-end">
             <WhoBox
               name="VENDOR"
               src={whitePlaceholder}
@@ -55,7 +55,7 @@ export default function Page() {
               instagram="instagram"
             />
           </div>
-          <div className="border-2 border-white md:col-start-4 md:col-span-2 md:row-start-1 md:self-start md:pt-12">
+          <div className="md:col-start-4 md:col-span-2 md:row-start-1 md:self-start md:pt-12">
             <WhoBox
               name="VENDOR"
               src={whitePlaceholder}
@@ -65,7 +65,7 @@ export default function Page() {
               instagram="instagram"
             />
           </div>
-          <div className="border-2 border-white md:col-start-5 md:col-span-2 md:row-start-2 md:self-end ">
+          <div className="md:col-start-5 md:col-span-2 md:row-start-2 md:self-end ">
             <WhoBox
               name="VENDOR"
               src={whitePlaceholder}
@@ -84,10 +84,10 @@ export default function Page() {
 function WhoBox({ src, name, alt, text, website, instagram }) {
   return (
     <>
-      <div className="grid grid-cols-2 grid-rows-3 w-full h-60 justify-center items-centerborder-2 border-white">
+      <div className="grid grid-cols-2 grid-rows-3 w-full h-60 justify-center">
         <div
           id="photo"
-          className="flex col-start-1 col-span-1 row-start-1 row-span-2 border-2 border-white"
+          className="flex col-start-1 col-span-1 row-start-1 row-span-2 "
         >
           <img
             className="object-fill"
@@ -96,7 +96,7 @@ function WhoBox({ src, name, alt, text, website, instagram }) {
         </div>
         <div
           id="vendor"
-          className="flex col-start-1 col-span-1 row-start-3 row-span-1 border-2 border-white"
+          className="flex col-start-1 col-span-1 row-start-3 row-span-1 "
         >
           <h2 className={`flex text-4xl items-center ${antonio.className}`}>
             {name}
@@ -104,13 +104,13 @@ function WhoBox({ src, name, alt, text, website, instagram }) {
         </div>
         <div
           id="text"
-          className="flex col-start-2 col-span-1 row-start-1 row-span-2 border-2 border-white"
+          className="flex col-start-2 col-span-1 row-start-1 row-span-2 "
         >
           <p className={`${josefin_sans.className} text-base`}>{text}</p>
         </div>
         <div
           id="links"
-          className="flex col-start-2 col-span-2 row-start-3 row-span-1 border-2 border-white"
+          className="flex col-start-2 col-span-2 row-start-3 row-span-1"
         >
           <p className="flex flex-col justify-center gap-0 items-start">
             <a
