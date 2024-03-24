24cd1736f2344bad0721f809a36cad4312358c9c
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-03-24 14:17:34 -0600

    added header to ticket pages

--- app/components/TicketBox.js
+++ app/components/TicketBox.js
@@ -2,13 +2,14 @@
 import Ticket from "@/components/Ticket";
 import Image from 'next/image'
 export default function TicketBox({selected,setSelected}){
+    
     const goToForm=()=>{
         let form = document.getElementById('infoForm')
         form.scrollIntoView({behavior:"smooth"})
     }
 
     return(
-        <div className={'h-[100vh] grid pt-[8vh] snap-start'} id={'ticketbox'}>
+        <div className={'h-[90vh] grid pt-[8vh] snap-start'} id={'ticketbox'}>
             <div className={'md:flex grid md:justify-around'} >
                 <Ticket selected={selected==='earlybird'} id={'earlybird'} select={()=>setSelected('earlybird')}>
                     <Image alt={'earlybird'} src={'/earlybird.png'} fill/>
