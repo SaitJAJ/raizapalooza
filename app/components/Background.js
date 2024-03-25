0edc5621234145e378a251a8cd2423bbc1cd2f2f
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-03-25 12:04:29 -0600

    fixed background z index for realsies this time

--- app/components/Background.js
+++ app/components/Background.js
@@ -3,7 +3,7 @@ import "../globals.css";
 export default function Background(props) {
   return (
     <>
-        <div className="relative w-full min-h-full bg-[url('../public/LandingBackground.png')] bg-repeat bg-opacity-50 -z-10">
+        <div className="relative w-full min-h-full bg-[url('../public/LandingBackground.png')] bg-repeat bg-opacity-50">
           <div className="">{props.children}</div>
         </div>
     </>
--- app/components/Landing.js
+++ app/components/Landing.js
@@ -5,19 +5,21 @@ import '../../styles/home.css';
 export default function Landing() {
   return (
     <>
-      <Background />
-      <div className="landing-inner">
-        <FancyTitle title={"Raizapalooza"}/>
-        <div className="info">
-          <div className='info-section'>
-            <p id="date-text">APRIL 13, 2024</p>
-          </div>
-          <div className='info-section'>
-            <p id='location-text' ><a href='https://www.scubajaysbar.com/' target='_blank'>@ Scuba Jay&apos;s Bar</a></p>
-            <p id='eighteen-only'>18+ only</p>
+      <Background>
+        <div className="landing-inner">
+          <FancyTitle title={"Raizapalooza"}/>
+          <div className="info">
+            <div className='info-section'>
+              <p id="date-text">APRIL 13, 2024</p>
+            </div>
+            <div className='info-section'>
+              <p id='location-text' ><a href='https://www.scubajaysbar.com/' target='_blank'>@ Scuba Jay&apos;s Bar</a></p>
+              <p id='eighteen-only'>18+ only</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Background>
+
     </>
 
   );
