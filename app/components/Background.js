f82eaf2a1770244d2777940d15f66277ae157971
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-03-25 10:28:46 -0600

    background image repeating but is overlayed over props.children

--- app/components/Background.js
+++ app/components/Background.js
@@ -1,11 +1,11 @@
 import "../globals.css";
 
-export default function Background() {
+export default function Background(props) {
   return (
     <>
-      <div className='background-images'>
-        <div className="w-full h-screen bg-[url('../public/LandingBackground.png')] bg-contain bg-repeat"></div>
-      </div>
+        <div className="absolute top-0 left-0 -z-10 w-full min-h-full bg-[url('../public/LandingBackground.png')] bg-repeat">
+          <div className="relative z-10">{props.children}</div>
+        </div>
     </>
   );
 }
