73719735a2f12a750b543f0998dd8ed8bcbaf63b
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-03-24 20:15:25 -0600

    More responsive tickets

--- app/components/Ticket.js
+++ app/components/Ticket.js
@@ -1,8 +1,8 @@
 'use client'
 export default function Ticket({select,selected = false,children}){
-    const selectedStyle='h-[30vh] md:h-[60vh] grow border-2 bg-background grow translate-x-[10px] translate-y-[-10px] transition-all relative'
-    const unselectedStyle = 'h-[30vh] md:h-[60vh] grow  bg-background border-2 box-border transition-all grayscale relative '
-    const style = ' h-[30vh] md:h-[60vh] mx-[5vw] border-2 grow'
+    const selectedStyle=' h-[450px] w-[300px] grow border-2 bg-background grow translate-x-[10px] translate-y-[-10px] transition-all relative'
+    const unselectedStyle = ' h-[450px] w-[300px] grow bg-background border-2 box-border transition-all grayscale relative '
+    const style = ' m-[2vw] w-fit h-fit border-2'
     return(
         <div className={style} onClick={select}>
             <div className={selected?selectedStyle:unselectedStyle} onClick={select}>
