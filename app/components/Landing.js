1ac4e17cbdad82714df214f751c21b06b587bff3
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-03-25 14:52:17 -0600

    added cta to home page

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -13,10 +13,11 @@ export default function Landing() {
               <p className='z-20' id="date-text">APRIL 13, 2024</p>
             </div>
             <div className='info-section'>
-              <p className='z-20 ' ><a className='text-element-2 text-4xl underline hover:no-underline font-bold' href='https://www.scubajaysbar.com/' target='_blank'>@ Scuba Jay&apos;s Bar</a></p>
+              <p className='z-20' ><a className='text-element-2 text-4xl underline hover:no-underline font-bold' href='https://www.scubajaysbar.com/' target='_blank'>@ Scuba Jay&apos;s Bar</a></p>
               <p className='z-20 text-2xl font-bold'>18+ only</p>
             </div>
           </div>
+          <button className=" bg-element-1 mt-5 z-10 p-5"><a className='text-element-2 font-extrabold font-josefin-sans max-lg:text-2xl text-4xl hover:underline' href='/tickets'>GET TICKETS NOW</a></button>
         </div>
 
     </>
